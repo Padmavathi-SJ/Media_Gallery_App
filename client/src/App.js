1f926e2d9421e4bd0ec6ed77856cabe8418c3a8c
padma <padmasj54@gmail.com>
2025-02-01 23:58:43 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes and Route
 import Home from './Components/Home';
-import Akila from './Components/Akila'; // Import Akila component
-import Padma from './Components/Padma'; // Import Padma component
-import Mom from './Components/Mom'; // Import Mom component
-import Dad from './Components/Dad'; // Import Dad component
 import EventsHome from './Components/EventsHome';
 import EventsGrid from './Components/EventsGrid';
 import Cursor from './Components/cursor';
@@ -17,10 +13,6 @@ function App() {
         <Cursor/>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/akila" element={<Akila />} />
-          <Route path="/padma" element={<Padma />} />
-          <Route path="/mom" element={<Mom />} />
-          <Route path="/dad" element={<Dad />} />
           <Route path="/events" element={<EventsHome />} />
           <Route path="/events/:eventId" element={<EventsGrid />} />
           <Route path="/cadets/:cadetId" element={<CadetGrid />} />
