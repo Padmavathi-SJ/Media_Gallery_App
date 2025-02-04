877d5a7ffe3a9ed142e18e72dda115e3d74d182c
padma <padmasj54@gmail.com>
2025-02-04 21:18:05 +0530

    upload updated

--- server/server.js
+++ server/server.js
@@ -1,10 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import router from './routes/router.js'; // Import main router
-import AkilaRouter from './routes/akilaRouter.js'; // Import Akila router
-import PadmaRouter from './routes/padmaRouter.js'; // Import Padma router
-import MomRouter from './routes/momRouter.js'; // Import Mom router
-import DadRouter from './routes/dadRouter.js'; 
 import deleteRouter from './routes/deleteRouter.js';
 import eventsRouter from './routes/eventRouter.js';
 import eventsHomeRouter from './routes/eventsHomeRouter.js';
@@ -23,10 +19,6 @@ app.use('/uploads', express.static('uploads')); // Serve images statically from 
 
 // Use the routes
 app.use('/api', router);
-app.use('/api/akila', AkilaRouter);
-app.use('/api/padma', PadmaRouter); // Use Padma router
-app.use('/api/mom', MomRouter); // Use Mom router
-app.use('/api/dad', DadRouter); // Use Dad router
 app.use('/api', deleteRouter);
 app.use('/api', eventsRouter);
 app.use('/api', eventsHomeRouter);
